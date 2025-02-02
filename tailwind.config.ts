59a0e2a64b94bcc443ab9c964a975911867a7dbc
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2025-02-02 10:51:19 +0530

    Update Tailwind configuration, modify global styles, add dependencies, and simplify homepage component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        btn_color: "var(--btn-color)",
       },
     },
   },
